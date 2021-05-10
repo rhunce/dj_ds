a88457f5eed66373cb404b6eabb02faf5a9b51c6
Raymond Hunce <hunce18c@gmail.com>
2021-05-10 16:58:37 -0400

    Create and register Sales models

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -1,5 +1,5 @@
 from .models import Profile
-from django.contrib.auto.models import User
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
